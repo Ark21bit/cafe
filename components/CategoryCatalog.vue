1409c2fc9c6dd16c86400aab4bbd3179e3502767
Ark21bit <webleif@vk.com>
2023-07-12 22:42:26 +0300

    upd

--- components/CategoryCatalog.vue
+++ components/CategoryCatalog.vue
@@ -2,8 +2,7 @@
     <div class="col-span-full overflow-hidden border-t border-white/10 py-8  lg:py-12">
         <div class="wrapper">
             <h2 class="flex gap-2.5 lg:gap-5 before:w-1 before:bg-[#618967] uppercase font-bold text-xl lg:text-3xl">{{ title }}</h2>
-            <Swiper :slidesPerView="1" :breakpoints="{ 640: { slidesPerView: 2 }, 1024: { slidesPerView: 4 } }"
-                class="swiper-category-catalog !overflow-visible mt-8 lg:mt-12" :spaceBetween="16">
+            <Swiper :slidesPerView="1" :breakpoints="{ 640: { slidesPerView: 2 }, 1024: { slidesPerView: 4 } }" class="swiper-category-catalog !overflow-visible mt-8 lg:mt-12" :spaceBetween="16">
                 <SwiperSlide v-for="item in products" :key="products.id" class="mr-4">
                     <CartCatalog :product="item" class="h-full" />
                 </SwiperSlide>
